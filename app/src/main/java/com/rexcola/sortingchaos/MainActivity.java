f01679ecbbbc4e497e04047ed735731df2376e67
James Spyker <jspyker@ca.ibm.com>
2019-01-04 12:22:18 -0500

    It works in the debugger!

--- app/src/main/java/com/rexcola/sortingchaos/MainActivity.java
+++ app/src/main/java/com/rexcola/sortingchaos/MainActivity.java
@@ -1,52 +1,86 @@
 package com.rexcola.sortingchaos;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.view.Menu;
+        import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    private static final int MENU_RESET = 1;
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+    private static final int MENU_STOP = 2;
+
+    private SortingChaosView sortingChaosView;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        super.onCreateOptionsMenu(menu);
+
+        menu.add(0, MENU_RESET, 0, R.string.menu_reset);
+        menu.add(0, MENU_STOP, 0, R.string.menu_stop);
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case MENU_RESET:
+                sortingChaosView.getThread().doReset();
+                return true;
+            case MENU_STOP:
+            	finish();
+                return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        return false;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // tell system to use the layout defined in our XML file
+        setContentView(R.layout.activity_main);
+
+        // get handles to the CatchView from XML, and its thread
+        sortingChaosView = findViewById(R.id.sortingChaosView);
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // just have the View save its state into our Bundle
+        super.onSaveInstanceState(outState);
     }
 }
